6b5b1ff294b7bed3f6b97eb286cb6414734341d4
Júlio Cavalcanti <jnc.nunescavalcanti@outlook.com>
2025-10-30 22:53:52 -0300

    Initial commit

---
+++ src/main/java/com/example/simple_user_crud/SimpleUserCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_user_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleUserCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simple_user_crud/SimpleUserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_user_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUserCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
